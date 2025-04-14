c7e60159fe85ed141629347b6487dac6e671bd4f
Bobby Lat <bobby.lat@makerx.com.au>
2025-04-14 17:50:01 +0800

    feat: allow valid oca to be specified for mock callbacks

--- src/application-spy.ts
+++ src/application-spy.ts
@@ -1,4 +1,6 @@
 import type { bytes, Contract } from '@algorandfoundation/algorand-typescript'
+import { OnCompleteAction } from '@algorandfoundation/algorand-typescript'
+import { getContractMethodAbiMetadata } from './abi-metadata'
 import type { ApplicationCallInnerTxnContext } from './impl/inner-transactions'
 import { methodSelector } from './impl/method-selector'
 import type { AnyFunction, ConstructorFor } from './typescript-helpers'
@@ -7,16 +9,16 @@ import { asNumber } from './util'
 export type AppSpyCb = (itxnContext: ApplicationCallInnerTxnContext) => void
 
 const predicates = {
-  bareCall: (cb: AppSpyCb): AppSpyCb => {
+  bareCall: (cb: AppSpyCb, ocas: OnCompleteAction[]): AppSpyCb => {
     return (ctx) => {
-      if (asNumber(ctx.numAppArgs) === 0) {
+      if (asNumber(ctx.numAppArgs) === 0 && ocas.includes(ctx.onCompletion)) {
         cb(ctx as ApplicationCallInnerTxnContext)
       }
     }
   },
-  methodSelector: (cb: AppSpyCb, selectorBytes: bytes): AppSpyCb => {
+  methodSelector: (cb: AppSpyCb, selectorBytes: bytes, ocas: OnCompleteAction[]): AppSpyCb => {
     return (ctx) => {
-      if (selectorBytes.equals(ctx.appArgs(0))) {
+      if (selectorBytes.equals(ctx.appArgs(0)) && ocas.includes(ctx.onCompletion)) {
         cb(ctx)
       }
     }
@@ -57,8 +59,17 @@ export class ApplicationSpy<TContract extends Contract = Contract> {
    * Registers a callback for a bare call (no arguments).
    * @param callback - The callback to be executed when a bare call is detected.
    */
-  onBareCall(callback: AppSpyCb) {
-    this.#spyFns.push(predicates.bareCall(callback))
+  onBareCall(callback: AppSpyCb): void
+  onBareCall(ocas: OnCompleteAction[], callback: AppSpyCb): void
+  onBareCall(...args: [AppSpyCb] | [OnCompleteAction[], AppSpyCb]): void {
+    let callback: AppSpyCb
+    let ocas: OnCompleteAction[] = [OnCompleteAction.NoOp]
+    if (args.length === 2) {
+      ;[ocas, callback] = args
+    } else {
+      ;[callback] = args
+    }
+    this.#spyFns.push(predicates.bareCall(callback, ocas))
   }
 
   /**
@@ -66,8 +77,18 @@ export class ApplicationSpy<TContract extends Contract = Contract> {
    * @param methodSignature
    * @param callback
    */
-  onAbiCall(methodSignature: bytes, callback: AppSpyCb) {
-    this.#spyFns.push(predicates.methodSelector(callback, methodSignature))
+  onAbiCall(methodSignature: bytes, callback: AppSpyCb): void
+  onAbiCall(methodSignature: bytes, ocas: OnCompleteAction[], callback: AppSpyCb): void
+  onAbiCall(...args: [bytes, AppSpyCb] | [bytes, OnCompleteAction[], AppSpyCb]): void {
+    let methodSignature: bytes
+    let callback: AppSpyCb
+    let ocas: OnCompleteAction[] = [OnCompleteAction.NoOp]
+    if (args.length === 3) {
+      ;[methodSignature, ocas, callback] = args
+    } else {
+      ;[methodSignature, callback] = args
+    }
+    this.#spyFns.push(predicates.methodSelector(callback, methodSignature, ocas))
   }
 
   private _tryGetMethod(name: string | symbol) {
@@ -85,8 +106,14 @@ export class ApplicationSpy<TContract extends Contract = Contract> {
         const fn = spy._tryGetMethod(methodName)
         if (fn === undefined) return fn
         return function (callback: AppSpyCb) {
+          let ocas: OnCompleteAction[] = [OnCompleteAction.NoOp]
+          if (spy.contract !== undefined) {
+            const metadata = getContractMethodAbiMetadata(spy.contract, methodName as string)
+            ocas = metadata.allowActions?.map((action) => OnCompleteAction[action]) ?? [OnCompleteAction.NoOp]
+          }
+
           const selector = methodSelector(fn, spy.contract)
-          spy.onAbiCall(selector, callback)
+          spy.onAbiCall(selector, ocas, callback)
         }
       },
     }) as _TypedApplicationSpyCallBacks<TContract>
