ecaba14f02fc0d814b0d893bd7d2c7d5f4eee1b3
p2arthur <arthurrabelopro@gmail.com>
2026-03-16 20:54:47 -0700

    refactor(docs): remove runtime examples collection in benefit of static mdx generated EXAMPLES DOCUMENTATION

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,23 +1,7 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
 import { docsLoader } from '@astrojs/starlight/loaders'
 import { docsSchema } from '@astrojs/starlight/schema'
-import { examplesLoader } from './loaders/examples-loader'
 
 export const collections = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-  examples: defineCollection({
-    loader: examplesLoader(),
-    schema: z.object({
-      id: z.string().optional(),
-      title: z.string(),
-      description: z.string(),
-      prerequisites: z.string(),
-      code: z.string(),
-      codeHtml: z.string(),
-      order: z.number().int().nonnegative(),
-      filename: z.string(),
-      runCommand: z.string(),
-      runCommandHtml: z.string(),
-    }),
-  }),
 }
